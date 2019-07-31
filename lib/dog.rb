671cee11e79940d4abc337075d31273c32d970d2
Chyrelle Lewis <chyrellelewis@gmail.com>
2019-07-31 19:38:47 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+# Your code goes here!
+class Dog
+  Dog.new
+end
+
+
+class bark
+  puts "woof!"
+end 
+
+def name=(dogs_name)
+  @this_dogs_name = dogs_name
+end
+end
+end
+
+  
+fido = Dog.new
+fido.name= "Fido"
+
+fido.name
+
+fido.bark
